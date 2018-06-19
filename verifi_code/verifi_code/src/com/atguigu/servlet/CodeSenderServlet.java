6010e937ba42cdd97e26dd7223b42fa7b26d8f87
lixingxing <40225711+lixingxing96@users.noreply.github.com>
2018-06-19 11:36:18 +0800

    Update CodeSenderServlet.java

--- verifi_code/verifi_code/src/com/atguigu/servlet/CodeSenderServlet.java
+++ verifi_code/verifi_code/src/com/atguigu/servlet/CodeSenderServlet.java
@@ -34,7 +34,54 @@ public class CodeSenderServlet extends HttpServlet {
 	 */
      
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
+		//1.获取参数,验空
+		//请求域中获取到电话号
+		String phoneNo = request.getParameter("phone_no");
+		//判断电话号是否为空
+		if(phoneNo==null) {
+			return ;
+		}
+		
+		//1.5  验证每个手机号每天最多发送三次
+		//取计数器
+		Jedis jedis = new Jedis("192.168.193.128",6379);
+		String countKey = VerifyCodeConfig.PHONE_PREFIX+phoneNo+VerifyCodeConfig.COUNT_SUFFIX;
+		String countStr = jedis.get(countKey);
+		
+		//计数器是否为空,创建计数器+1
+		if(countStr==null) {
+			jedis.setex(countKey, VerifyCodeConfig.SECONDS_PER_DAY, "1");
+		}else {
+			//如果不为空,判断计数器次数,如果=3返回
+			int count  = Integer.parseInt(countStr);
+			if(count>=VerifyCodeConfig.COUNT_TIMES_1DAY) {
+				response.getWriter().print("limit");
+				jedis.close();
+				return ;
+			}else {
+				//计数器小于3,则+1
+				jedis.incr(countKey);
+			}
+		}
+		
+		//2.获取验证码
+		String code = genCode(VerifyCodeConfig.CODE_LEN);
+		
+		//3.保存验证码
+		//Jedis jedis = new Jedis("192.168.193.128",6379);
+		//获取key值
+		String codeKey = VerifyCodeConfig.PHONE_PREFIX+phoneNo+VerifyCodeConfig.PHONE_SUFFIX;
+		//key:codeKey   时间:VerifyCodeConfig.CODE_TIMEOUT   value:code
+		jedis.setex(codeKey, VerifyCodeConfig.CODE_TIMEOUT, code);
+		
+		//记得要关闭jedis
+		jedis.close();
+		//4.发送验证码
+		System.out.println(code);
+		
+		//5.返回
+		response.getWriter().print(true);
+		  
 		  
 		 
 		
